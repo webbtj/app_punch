d3a1d6fac348c12045c2767f48b127c97f6f9a51
robmoore902 <capital.r@gmail.com>
2014-12-17 13:01:35 -0400

    Rearranged code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,35 +2,13 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).ready(function() {
 	//todo retreive list of people and print them
+	var people = [];
 	$.ajax({
 		url:"http://localhost:1337/Person/find",
 		success:function(result){
-			var text = '';
-			if(result.length > 0) {
-				text = "<section class='person' id='firstPerson'>" +
-				"<div class='in'></div>" +
+			people = result;
+			findPeople(people);
 
-				"<div class='click-response'></div>" +
-
-				"<img class='img-in' src='assets/img/icon_jeff.svg'>" +
-
-				"<h1>"+result[0].name+"</h1>" +
-
-				"</section>";
-				for(var i = 1; i<result.length; i++) {
-					text = text + "<section class='person'>" +
-						"<div class='in'></div>" +
-
-						"<div class='click-response'></div>" +
-
-						"<img class='img-in' src='assets/img/icon_jeff.svg'>" +
-
-						"<h1>"+result[i].name+"</h1>" +
-
-						"</section>"
-				}
-				$("#people").append(text);
-			}
 			// OnClick =====================================================================
 			$('.person').click(function() {
 
@@ -78,8 +56,73 @@ $(document).ready(function() {
 				}, 200);
 
 			});
+			console.log('finding;');
+			console.log(people);
+			setInterval(function() {console.log('doing something.'); findPeople(people);}, 5000);
 		}
 	}); //end of user.find and binding clicks
+
+
+	function findPeople(result) {
+		var text = '';
+		console.log('find people.');
+		if(result.length > 0) {
+			text = "<section class='person' id='firstPerson'>" +
+			"<div class='in'></div>" +
+
+			"<div class='click-response'></div>" +
+
+			"<img class='img-in' src='assets/img/icon_jeff.svg'>" +
+
+			"<h1>"+result[0].name+"</h1>" +
+
+			"</section>";
+			for(var i = 1; i<result.length; i++) {
+				text = text + "<section class='person'>" +
+				"<div class='in'></div>" +
+
+				"<div class='click-response'></div>" +
+
+				"<img class='img-in' src='assets/img/icon_jeff.svg'>" +
+
+				"<h1>"+result[i].name+"</h1>" +
+
+				"</section>"
+			}
+			$("#people").html(text);
+		}
+	}
+
+	
+	//function findPeople() {
+	//	var text = '';
+	//	console.log('find people.');
+	//	if(people.length > 0) {
+	//		text = "<section class='person' id='firstPerson'>" +
+	//		"<div class='in'></div>" +
+    //
+	//		"<div class='click-response'></div>" +
+    //
+	//		"<img class='img-in' src='assets/img/icon_jeff.svg'>" +
+    //
+	//		"<h1>"+people[0].name+"</h1>" +
+    //
+	//		"</section>";
+	//		for(var i = 1; i<people.length; i++) {
+	//			text = text + "<section class='person'>" +
+	//			"<div class='in'></div>" +
+    //
+	//			"<div class='click-response'></div>" +
+    //
+	//			"<img class='img-in' src='assets/img/icon_jeff.svg'>" +
+    //
+	//			"<h1>"+people[i].name+"</h1>" +
+    //
+	//			"</section>"
+	//		}
+	//		$("#people").append(text);
+	//	}
+	//}
 });
 $(document).foundation();
 
